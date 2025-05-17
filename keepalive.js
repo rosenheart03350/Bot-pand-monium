8af59aabba8d692136451996a7b39cd655ff1866
rosenheart03350 <cuillerat.alban@laposte.net>
2025-05-17 23:20:23 +0200

    Create keepalive.js

---
+++ keepalive.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Bot is alive!'));
+
+app.listen(3000, () => console.log('Keepalive server running on port 3000'));
